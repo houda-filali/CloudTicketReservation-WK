0ec0939d901a445af21802a1c8ef210a02f0b2e0
Yuioytffhio <hiba.tal05@gmail.com>
2026-03-02 17:36:59 -0500

    Added junit 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.cloudticketreservationwk"
-    compileSdk = 34
+    compileSdk = 36 // changed from 34 to 36
 
     defaultConfig {
         applicationId = "com.example.cloudticketreservationwk"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36 // changed from 34 to 36
         versionCode = 1
         versionName = "1.0"
 
@@ -38,12 +38,21 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
 
-    // JUnit 4 only (remove JUnit 5 dependencies)
-    testImplementation("junit:junit:4.13.2")
+    // We're using junit 5, its in the requirements
+    //testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+
+    // add this for junit
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
+
 
     // Android Test dependencies
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
 
-// Remove the tasks.withType<Test> block for JUnit 4
+// DON'T REMOVE (for junit5)
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
